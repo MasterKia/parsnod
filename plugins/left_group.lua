0ec66e4ef241f5d80b974a74cef93bf4761a55b5
MasterKia <mysignup27@gmail.com>
2016-03-17 09:38:28 +0330

    Update left_group.lua

--- plugins/left_group.lua
+++ plugins/left_group.lua
@@ -6,7 +6,7 @@ local bot_id = 182195776 -- your bot id
     if matches[1] == 'leavenod32' and is_admin(msg) then
        chat_del_user("chat#id"..msg.to.id, 'user#id'..bot_id, ok_cb, false)
     elseif msg.action.type == "chat_add_user" and msg.action.user.id == tonumber(bot_id) and not is_sudo(msg) then
-      send_large_msg("chat#id"..msg.to.id, 'this is not one of my groups fuck you (coming soon u Globall Banned and Blocked of this bot KOSE NANAT️).', ok_cb, false)
+      send_large_msg("chat#id"..msg.to.id, 'برای ادد کردن ربات به گروه با سودو تماس بگیرید', ok_cb, false)
       chat_del_user("chat#id"..msg.to.id, 'user#id'..bot_id, ok_cb, false)
       block_user("user#id"..msg.from.id,ok_cb,false)
     end
