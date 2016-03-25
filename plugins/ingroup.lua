f2c05b8531900c190545dbc9eeca2a565ab9d900
MasterKia <mysignup27@gmail.com>
2016-03-25 14:16:35 +0430

    Update ingroup.lua

--- plugins/ingroup.lua
+++ plugins/ingroup.lua
@@ -220,7 +220,7 @@ local function show_group_settingsmod(msg, data, target)
     	leave_ban = data[tostring(msg.to.id)]['settings']['leave_ban']
    	end
   local settings = data[tostring(target)]['settings']
-  local text = "تنظیمات گروه:\n جلوگیری از ورود با لینک : "..settings.lock_join.."\nقفل فحش : "..settings.antifosh.."\nقفل چت : "..settings.lock_chat.."\n•••قفل تبلیغات : "..settings.antiads.."\nقفل اسم گروه : "..settings.lock_name.."\nقفل عکس گروه : "..settings.lock_photo.."\nقفل ادد : "..settings.lock_member.."\nقفل لیو : "..leave_ban.."\nحساسیت ضداسپم : "..NUM_MSG_MAX.."\nجلوگیری از ادد کردن ربات : "..bots_protection.."\n >>Protected By ParsTG :)
+  local text = "›Group Settings:\n•••Kick new member with link : "..settings.lock_join.."\n•••Lock group Fosh : "..settings.antifosh.."\n•••Lock group chat : "..settings.lock_chat.."\n•••Lock group ads : "..settings.antiads.."\n•••Lock group name : "..settings.lock_name.."\n•••Lock group photo : "..settings.lock_photo.."\n•••kick new member : "..settings.lock_member.."\n•••Lock leave ban : "..leave_ban.."\n•••flood set on : "..NUM_MSG_MAX.."\n•••Bot can come : "..bots_protection.."                                        ›»Nod32 People Version 7.8 (◉_◉)"
   return text
 end
 
