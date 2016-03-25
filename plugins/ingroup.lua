f9cd638c4e348ca01ac315bbcebdec9c678fc376
MasterKia <mysignup27@gmail.com>
2016-03-25 14:03:39 +0430

    Update ingroup.lua

--- plugins/ingroup.lua
+++ plugins/ingroup.lua
@@ -101,7 +101,7 @@ function check_member_group(cb_extra, success, result)
       end
       data[tostring(groups)][tostring(msg.to.id)] = msg.to.id
       save_data(_config.moderation.data, data)
-      return send_large_msg(receiver, 'You have been promoted as the owner.')
+      return send_large_msg(receiver, 'شما به عنوان صاحب گروه انتخاب شدید !')
     end
   end
 end
@@ -137,7 +137,7 @@ local function check_member_modadd(cb_extra, success, result)
       end
       data[tostring(groups)][tostring(msg.to.id)] = msg.to.id
       save_data(_config.moderation.data, data)
-      return send_large_msg(receiver, 'Group is added and you have been promoted as the owner ')
+      return send_large_msg(receiver, 'گروه ادد شد و شما به عنوان صاحب گروه انتخاب شدید ! ')
     end
   end
 end
@@ -220,7 +220,7 @@ local function show_group_settingsmod(msg, data, target)
     	leave_ban = data[tostring(msg.to.id)]['settings']['leave_ban']
    	end
   local settings = data[tostring(target)]['settings']
-  local text = "›Group Settings:\n•••Kick new member with link : "..settings.lock_join.."\n•••Lock group Fosh : "..settings.antifosh.."\n•••Lock group chat : "..settings.lock_chat.."\n•••Lock group ads : "..settings.antiads.."\n•••Lock group name : "..settings.lock_name.."\n•••Lock group photo : "..settings.lock_photo.."\n•••kick new member : "..settings.lock_member.."\n•••Lock leave ban : "..leave_ban.."\n•••flood set on : "..NUM_MSG_MAX.."\n•••Bot can come : "..bots_protection.."                                        ›»Nod32 People Version 7.8 (◉_◉)"
+  local text = "›تنظیمات گروه:\nقفل اسم گروه : "..settings.lock_name.."\nقفل عکس گروه : "..settings.lock_photo.."\nقفل ادد : "..settings.lock_member.."\nجلوگیری از ورود با لینک : "..settings.lock_join.."\nقفل تبلیغات : "..settings.antiads.."\nقفل چت : "..settings.lock_chat.."\nقفل فحش : "..settings.antifosh.."\nقفل لیو : "..leave_ban.."\nحساسیت ضد اسپم : "..NUM_MSG_MAX.."\nجلوگیری از ورود ربات : "..bots_protection..\n >>>Pars"
   return text
 end
 
